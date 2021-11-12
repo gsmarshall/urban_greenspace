04de409ac9ab9d06079deb4274f1d670a4f5b6ee
Sam Marshall <79521697+gsmarshall@users.noreply.github.com>
2021-11-11 20:54:34 -0500

    Progress towards running analysis

--- analysis.js
+++ analysis.js
@@ -1,50 +1,64 @@
-var visParamsMax = {
-  bands: ['N_max', 'R_max', 'G_max'],
-  min: 0,
-  max: 255
-};
+class Preprocessing {
 
-var manchester =  ee.Feature(
-            ee.Geometry.Polygon([[
-              [-71.54293288787831,42.926820038499166],
-              [-71.35822524627675,42.926820038499166],
-              [-71.35822524627675,43.0594105256263],
-              [-71.54293288787831,43.0594105256263],
-              [-71.54293288787831,42.926820038499166]
-            ]]),
-            {'system:index': '0'}
-        );
+  constructor(){
+    // true color visualization parameters for NAIP image
+    var visParamsMax = {
+      bands: ['N_max', 'R_max', 'G_max'],
+      min: 0,
+      max: 255
+    };
+
+    // study region of Manchester, NH explicitly established here - hope to make dynamic later
+    var manchester =  ee.Feature(
+                ee.Geometry.Polygon([[
+                  [-71.54293288787831,42.926820038499166],
+                  [-71.35822524627675,42.926820038499166],
+                  [-71.35822524627675,43.0594105256263],
+                  [-71.54293288787831,43.0594105256263],
+                  [-71.54293288787831,42.926820038499166]
+                ]]),
+                {'system:index': '0'}
+            );
+
+    var preprocessed = ee.Image();
+  }
 
 
-// filter by images with 4 bands and calculates NDVI
-// returns null if image has less than 4 bands, returns image plus ndvi band for valid 4 band images
-var band_filter = function(image){
-  return ee.Algorithms.If(image.bandNames().length().eq(4), image.addBands(image.normalizedDifference(['N', 'R']).rename('NDVI')), null);
-};
-// future: try to buffer study area by a percentage of its width to reduce edge effects
-// also will give more flexibility to dynamically display the data in our app
-var bounds = manchester.bounds().coordinates().get(0);
+  // filter by images with 4 bands and calculates NDVI
+  // returns null if image has less than 4 bands, returns image plus ndvi band for valid 4 band images
+  band_filter(image){
+    return ee.Algorithms.If(image.bandNames().length().eq(4), image.addBands(image.normalizedDifference(['N', 'R']).rename('NDVI')), null);
+  };
+  // future: try to buffer study area by a percentage of its width to reduce edge effects
+  // also will give more flexibility to dynamically display the data in our app
+  // var bounds = manchester.bounds().coordinates().get(0);
 
-// can use ee.Geometry.Point(-71.45, 42.99) instead of study area - not quite perfect, but will work for now
-// returns composite image of all 4-band naip tiles within the study area
-var preprocess = function(study_area){
-  //buffer_dist = manchester.bounds().coordinates();
+  // can use ee.Geometry.Point(-71.45, 42.99) instead of study area - not quite perfect, but will work for now
+  // returns composite image of all 4-band naip tiles within the study area
+  preprocess(){
+    //buffer_dist = manchester.bounds().coordinates();
+    study_area = this.manchester;
 
-  // mapping over featurecollection is slower than filtering, but I haven't been able to get filtering by bands to work
-  var naip_4band = ee.ImageCollection('USDA/NAIP/DOQQ').filterBounds(study_area).map(band_filter, true);
+    // mapping over featurecollection is slower than filtering, but I haven't been able to get filtering by bands to work
+    var naip_4band = ee.ImageCollection('USDA/NAIP/DOQQ').filterBounds(study_area).map(band_filter, true);
 
-  // forms a composite image based on several measures from all images in the collection - not sure how these composites will figure in the
-  // classification but we'll keep them for now
-  var composite = naip_4band.reduce(ee.Reducer.max())
-                // .addBands(naip_4band.reduce(ee.Reducer.mean()))   // include a mean reducer
-                // .addBands(naip_4band.reduce(ee.Reducer.percentile([20])))// include a 20th percentile reducer
-                // .addBands(naip_4band.reduce(ee.Reducer.max()))// include a standard deviation reducer
-                .float();
+    // forms a composite image based on several measures from all images in the collection - not sure how these composites will figure in the
+    // classification but we'll keep them for now
+    var composite = naip_4band.reduce(ee.Reducer.max())
+                  // .addBands(naip_4band.reduce(ee.Reducer.mean()))   // include a mean reducer
+                  // .addBands(naip_4band.reduce(ee.Reducer.percentile([20])))// include a 20th percentile reducer
+                  // .addBands(naip_4band.reduce(ee.Reducer.max()))// include a standard deviation reducer
+                  .float();
 
-  return composite.clip(study_area);
-};
+    return composite.clip(study_area);
+  };
 
-// var composite2 = preprocess(manchester);
-export default function analysis({
-  this.composite2 = preprocess(manchester);
-})
+  // var composite2 = preprocess(manchester);
+  // export default function analysis(){
+  //   return preprocess(manchester);
+  // }
+}
+// ***************** end class Preprocessing *******************
+
+
+module.export = {Preprocessing}
--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 /**
- * @license
- * Copyright 2020 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
  * @fileoverview Default entry point for App Engine Node.js runtime. Defines a
  * web service which returns the mapid to be used by clients to display map
  * tiles showing slope computed in real time from SRTM DEM data. See
  * accompanying README file for instructions on how to set up authentication.
  */
+
+ // SyntaxError: Cannot use import statement outside a module
+// import analysis from './analysis.js';
+
 const ee = require('@google/earthengine');
 const express = require('express');
 const privateKey = require('./.private-key.json');
 const port = process.env.PORT || 3000;
 
-const analysis = require('./analysis.js');
+// const analysis = require('./analysis.js');
+// const analysis_alt = require('./analysis_alt.js');
 
 var application_root = __dirname;
 
@@ -23,26 +22,17 @@ var application_root = __dirname;
 // Define endpoint at /mapid.
 const app = express().get('/mapid', (_, response) => {
   console.log('do Earth Engine API using private key...');
-  // var manchester =  ee.Feature(
-  //             ee.Geometry.Polygon([[
-  //               [-71.54293288787831,42.926820038499166],
-  //               [-71.35822524627675,42.926820038499166],
-  //               [-71.35822524627675,43.0594105256263],
-  //               [-71.54293288787831,43.0594105256263],
-  //               [-71.54293288787831,42.926820038499166]
-  //             ]]),
-  //             {'system:index': '0'}
-  //         );
+
 
   var visParams = {
     bands: ['R', 'G', 'B'],
     min: 0,
     max: 255
     };
-  // const srtm = ee.Image('CGIAR/SRTM90_V4');
-  // const slope = ee.Terrain.slope(srtm);
-  const naip = ee.ImageCollection('USDA/NAIP/DOQQ').filterBounds(ee.Geometry.Point(-71.45, 42.99)).sort('system:time_start', false).first();
-  naip.getMap(visParams, ({mapid}) => response.send(mapid));
+
+  const manch_pre = new Preprocessing();
+  const composite2 = manch_pre.preprocess();
+  composite2.getMap(visParams, ({mapid}) => response.send(mapid));
   // slope.getMap({min: 0, max: 60}, ({mapid}) => response.send(mapid));
 });
 
@@ -76,7 +66,62 @@ Visit https://developers.google.com/earth-engine/service_account#how-do-i-create
 
 
 
-  // ************************* filter by band availability **********************
+  class Preprocessing {
+
+    constructor(){
+      // true color visualization parameters for NAIP image
+      var visParamsMax = {
+        bands: ['N_max', 'R_max', 'G_max'],
+        min: 0,
+        max: 255
+      };
+
+      // study region of Manchester, NH explicitly established here - hope to make dynamic later
+      var manchester =  ee.Feature(
+                  ee.Geometry.Polygon([[
+                    [-71.54293288787831,42.926820038499166],
+                    [-71.35822524627675,42.926820038499166],
+                    [-71.35822524627675,43.0594105256263],
+                    [-71.54293288787831,43.0594105256263],
+                    [-71.54293288787831,42.926820038499166]
+                  ]]),
+                  {'system:index': '0'}
+              );
+
+      var preprocessed = ee.Image();
+    }
+
+
+    // filter by images with 4 bands and calculates NDVI
+    // returns null if image has less than 4 bands, returns image plus ndvi band for valid 4 band images
+    band_filter(image){
+      return ee.Algorithms.If(image.bandNames().length().eq(4), image.addBands(image.normalizedDifference(['N', 'R']).rename('NDVI')), null);
+    }
+    // future: try to buffer study area by a percentage of its width to reduce edge effects
+    // also will give more flexibility to dynamically display the data in our app
+    // var bounds = manchester.bounds().coordinates().get(0);
+
+    // can use ee.Geometry.Point(-71.45, 42.99) instead of study area - not quite perfect, but will work for now
+    // returns composite image of all 4-band naip tiles within the study area
+    preprocess(){
+      //buffer_dist = manchester.bounds().coordinates();
+      var study_area = this.manchester;
+
+      // mapping over featurecollection is slower than filtering, but I haven't been able to get filtering by bands to work
+      var naip_4band = ee.ImageCollection('USDA/NAIP/DOQQ').filterBounds(study_area).map(this.band_filter, true);
+
+      // forms a composite image based on several measures from all images in the collection - not sure how these composites will figure in the
+      // classification but we'll keep them for now
+      var composite = naip_4band.reduce(ee.Reducer.max())
+                    // .addBands(naip_4band.reduce(ee.Reducer.mean()))   // include a mean reducer
+                    // .addBands(naip_4band.reduce(ee.Reducer.percentile([20])))// include a 20th percentile reducer
+                    // .addBands(naip_4band.reduce(ee.Reducer.max()))// include a standard deviation reducer
+                    .float();
+
+      return composite.clip(study_area);
+    }
+  }
+
   // python for getting study area
   // geometry = None
   //   if self.request.get('rectangle'):
